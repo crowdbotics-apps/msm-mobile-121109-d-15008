aff964780f1dce55903ee90b878ead2cf9667dac
Crowdbotics <team@crowdbotics.com>
2020-11-12 07:47:11 +0000

    Update components

--- src/features/BlankScreen014443/screens/blank.js
+++ src/features/BlankScreen014443/screens/blank.js
@@ -33,11 +33,13 @@ export default class Blank extends React.Component {
         onPress={() => alert("Pressed!")}
       />
       <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
         style={styles.Switch_3}
         value={this.state.Switch_3}
         onValueChange={nextChecked => this.setState({ Switch_3: nextChecked })}
       />
       <DateTimePicker
+        showIcon={false}
         style={styles.DateTimePicker_4}
         date={this.state.DateTimePicker_4}
         onDateChange={selectedDate =>
@@ -61,5 +63,14 @@ const styles = StyleSheet.create({
   View_1: {},
   Button_2: { width: 100, height: 29, paddingTop: 10, borderWidth: 5 },
   Switch_3: { width: 100, height: 28, alignSelf: "center", borderWidth: 3 },
-  DateTimePicker_4: { width: 100, alignSelf: "flex-end", borderWidth: 2 }
+  DateTimePicker_4: { width: 100, alignSelf: "flex-end", borderWidth: 2 },
+  View_1: {},
+  Button_2: { width: 100, height: 29, paddingTop: 10, borderWidth: 5 },
+  Switch_3: { width: 100, height: 28, alignSelf: "center", borderWidth: 3 },
+  DateTimePicker_4: {
+    width: 100,
+    alignSelf: "flex-end",
+    borderWidth: 0,
+    borderBottomWidth: 2
+  }
 })
